f3a0900a9dd17e3c6359d2744a0705c9787d7bf2
shankz <official.shankarshaw@gmail.com>
2023-07-30 20:48:06 +0530

    Added Security with JWT

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -3,6 +3,7 @@ package com.luv2code.springbootlibrary.controller;
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.service.BookService;
 import com.luv2code.springbootlibrary.service.impl.BookServiceImpl;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,25 +15,25 @@ public class BookController {
     private BookService bookService;
 
     @Autowired
-    public BookController(BookServiceImpl bookService){
-        this.bookService=bookService;
+    public BookController(BookServiceImpl bookService) {
+        this.bookService = bookService;
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception{
-        String userEmail = "shankarshaw023@gmail.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) throws Exception{
-        String userEmail = "shankarshaw023@gmail.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.checkedoutByTheUser(userEmail, bookId);
     }
 
     @GetMapping("/secure/currentloans/count")
-    public Integer currentLoansCount() throws Exception{
-        String userEmail = "shankarshaw023@gmail.com";
+    public Integer currentLoansCount(@RequestHeader(value = "Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 }
